8fe9f84bc7274f3b516d3170d95f546b2f9a7cf6
unknown <kozak.balint15@gmail.com>
2024-10-03 19:52:40 +0200

    feat(api): add cors middleware

--- api/cmd/api/healthcheck.go
+++ api/cmd/api/healthcheck.go
@@ -7,9 +7,10 @@ import (
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	env := envelope{
 		"status": "available",
-		"system_info": map[string]string{
-			"environment": app.config.env,
-			"version":     version,
+		"system_info": map[string]interface{}{
+			"environment":          app.config.env,
+			"cors_trusted_origins": app.config.cors.trustedOrigins,
+			"version":              version,
 		},
 	}
 
--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -28,6 +29,9 @@ type config struct {
 	jwt struct {
 		secret string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -47,6 +51,10 @@ func main() {
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
 	flag.StringVar(&cfg.jwt.secret, "jwt-secret", os.Getenv("JWT_SECRET"), "JWT secret")
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
