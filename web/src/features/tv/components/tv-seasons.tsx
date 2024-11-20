796742e8046f8ad78aa77034c0e807096e95f6f9
Bálint Kozák <kozak.balint15@gmail.com>
2024-11-20 20:43:51 +0100

    feat(web): season browser on tv-show layout

--- web/src/features/tv/components/tv-seasons.tsx
+++ web/src/features/tv/components/tv-seasons.tsx
@@ -1,7 +1,17 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from '@/components/ui/select';
 import { useGetTvSeasonsById } from '../api/get-tv-seasons-by-id';
+import { useState } from 'react';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
 
 export const TvSeasons = ({ tvId }: { tvId: string }) => {
   const tvSeasonsQuery = useGetTvSeasonsById({ id: tvId });
+  const [selectedSeason, setSelectedSeason] = useState<number>(1);
 
   if (tvSeasonsQuery.isLoading) {
     return <div>Loading...</div>;
@@ -14,19 +24,57 @@ export const TvSeasons = ({ tvId }: { tvId: string }) => {
   }
 
   return (
-    <div className="flex flex-col">
-      <div className="text-2xl font-bold">Seasons</div>
-      <div className="grid grid-cols-3 gap-4">
-        {seasons.seasons.map((seasons) => (
-          <div key={seasons.season_number} className="flex flex-col">
-            <img
-              src={seasons.poster_url}
-              alt={seasons.name}
-              className="w-full h-64 object-cover"
-            />
-            <div className="text-lg font-bold">{seasons.name}</div>
-            <div className="text-lg">{seasons.episode_count} episodes</div>
-          </div>
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-row place-items-center gap-2">
+        <div className="text-2xl font-bold">Seasons</div>
+        <div className="text-xl font-thin">({seasons.number_of_seasons})</div>
+      </div>
+      <Select
+        onValueChange={(value) => {
+          const seasonNumber = parseInt(value);
+          setSelectedSeason(seasonNumber);
+        }}
+      >
+        <SelectTrigger className="w-[180px]">
+          Season {selectedSeason}
+        </SelectTrigger>
+        <SelectContent>
+          {seasons.seasons.map((season) => (
+            <SelectItem
+              key={season.season_number}
+              value={season.season_number.toString()}
+            >
+              {season.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <div className="flex flex-col gap-2 max-h-96 overflow-scroll">
+        {seasons.seasons[selectedSeason - 1].episodes.map((episode) => (
+          <Card className="flex flex-row h-32 w-full">
+            <CardHeader className="p-0 min-w-fit">
+              <img
+                src={episode.still_url}
+                alt={episode.name}
+                className="h-full w-full rounded-xl"
+              />
+            </CardHeader>
+            <CardContent className="py-2 w-full">
+              <div className="flex flex-row gap-4">
+                <div className="flex flex-col">
+                  <div className="flex flex-row gap-2 font-bold text-xl">
+                    <div>{episode.episode_number}.</div>
+                    <div>{episode.name}</div>
+                  </div>
+                  <div className="line-clamp-3">{episode.overview}</div>
+                </div>
+                <div className="flex flex-row gap-2 flex-grow justify-end">
+                  <Button size={'icon'} variant={'default'}></Button>
+                  <Button size={'icon'} variant={'default'}></Button>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
         ))}
       </div>
     </div>
