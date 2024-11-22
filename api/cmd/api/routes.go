a5d2d6d3e1cbe05a8d5a9f07d63eaae916c94448
Bálint Kozák <kozak.balint15@gmail.com>
2024-11-22 19:07:09 +0100

    chore(api): require authenticated users on all possible route

--- api/cmd/api/routes.go
+++ api/cmd/api/routes.go
@@ -14,26 +14,26 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/users/me", app.requireAuthenticatedUser(app.getRequestUserHandler))
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.createUserHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/users/authenticate", app.createAuthenticationTokenHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/users/logout", app.invalidateAuthenticationTokenHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/users/logout", app.requireAuthenticatedUser(app.invalidateAuthenticationTokenHandler))
 
-	router.HandlerFunc(http.MethodGet, "/v1/search/movies", app.searchMoviesHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/search/tv", app.searchTvHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/search/people", app.searchPeopleHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/search/movies", app.requireAuthenticatedUser(app.searchMoviesHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/search/tv", app.requireAuthenticatedUser(app.searchTvHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/search/people", app.requireAuthenticatedUser(app.searchPeopleHandler))
 
-	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.getMovieByIdHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.requireAuthenticatedUser(app.getMovieByIdHandler))
 
-	router.HandlerFunc(http.MethodGet, "/v1/tv/:id", app.getTvByIdHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/tv/:id/seasons", app.getTvSeasonsHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/tv/:id/seasons/:season/episodes", app.getTvEpisodesHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/tv/:id/seasons/:season/episodes/:episode", app.getTvEpisodeHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/tv/:id", app.requireAuthenticatedUser(app.getTvByIdHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/tv/:id/seasons", app.requireAuthenticatedUser(app.getTvSeasonsHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/tv/:id/seasons/:season/episodes", app.requireAuthenticatedUser(app.getTvEpisodesHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/tv/:id/seasons/:season/episodes/:episode", app.requireAuthenticatedUser(app.getTvEpisodeHandler))
 
-	router.HandlerFunc(http.MethodGet, "/v1/people/:id", app.getPersonByIdHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/people/:id", app.requireAuthenticatedUser(app.getPersonByIdHandler))
 
-	router.HandlerFunc(http.MethodGet, "/v1/cast/movies/:id", app.getMovieCastHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/cast/tv/:id", app.getTvCastHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/cast/movies/:id", app.requireAuthenticatedUser(app.getMovieCastHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/cast/tv/:id", app.requireAuthenticatedUser(app.getTvCastHandler))
 
-	router.HandlerFunc(http.MethodGet, "/v1/watch/movies/:id", app.watchMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/watch/tv/:id", app.watchTvHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/watch/movies/:id", app.requireAuthenticatedUser(app.watchMovieHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/watch/tv/:id", app.requireAuthenticatedUser(app.watchTvHandler))
 
 	return app.recoverPanic(app.enableCORS(app.authenticate(router)))
 }
