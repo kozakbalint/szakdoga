ddcfc2dd629045882c60eb14eb45e65d59c79685
Bálint Kozák <kozak.balint15@gmail.com>
2024-12-16 16:11:42 +0100

    fix(db): typo

--- api/migrations/000013_create_watched_episodes_table.up.sql
+++ api/migrations/000013_create_watched_episodes_table.up.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS watched_episodes (
     WITH
         TIME ZONE NOT NULL DEFAULT NOW (),
         FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-        FOREIGN KEY (episode_id) REFERENCES tv_shows_episodes (id) ON DELETE CASCADE,
+        FOREIGN KEY (episode_id) REFERENCES tv_shows_episodes (id) ON DELETE CASCADE
 );
