0c00f1ff8f572363613c44fde68777242ca61382
Bálint Kozák <kozak.balint15@gmail.com>
2025-01-31 12:23:50 +0100

    improvement(web): parallel fetch multiple queries

---
+++ web/src/features/cast/components/top-cast.tsx
@@ -0,0 +1,41 @@
+import { ChevronRight } from 'lucide-react';
+import { Link } from '@tanstack/react-router';
+import { Cast } from './cast';
+import { CastMovies, CastTv } from '@/types/types.gen';
+
+export const TopCast = ({
+  id,
+  isTv,
+  data,
+}: {
+  id: string;
+  isTv: boolean;
+  data: CastMovies[] | CastTv[];
+}) => {
+  const topCast = data.slice(0, 10);
+  const link = isTv ? '/app/cast/tv/' : '/app/cast/movie/';
+
+  return (
+    <div className="flex flex-col gap-4 max-h-[370px] overflow-hidden">
+      <Link to={link + id} resetScroll={true} className="hover:underline">
+        <div className="flex flex-row gap-1 place-items-center text-2xl font-semibold">
+          <p>
+            Cast <span className="text-xl font-thin">({data.length})</span>
+          </p>
+          <ChevronRight size={32} className="align-baseline" />
+        </div>
+      </Link>
+      <div className="flex flex-row flex-wrap max-w-full gap-4 justify-center lg:justify-start overflow-hidden">
+        {topCast.map((actor) => (
+          <Link
+            key={actor.id}
+            to={`/app/people/` + actor.id}
+            resetScroll={true}
+          >
+            <Cast key={actor.id} actor={actor} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
--- web/src/features/tv/components/tv-view.tsx
+++ web/src/features/tv/components/tv-view.tsx
@@ -1,15 +1,39 @@
-import { TvCast } from '@/features/cast/components/tv-cast';
-import { TvHeader } from './tv-header';
+import { TvHeader, TvHeaderData } from './tv-header';
 import { TvSeasons } from './tv-seasons';
 import { NextEpisode } from './tv-next-episode';
+import { getTvDetailsQueryOptions } from '../api/get-tv-details';
+import { useSuspenseQueries } from '@tanstack/react-query';
+import { isTvOnWatchlistQueryOptions } from '@/features/watchlist/api/is-tv-on-watchlist';
+import { getTvWatchProvidersQueryOptions } from '@/features/watchproviders/api/get-tv-watch-providers';
+import { getTvCastQueryOptions } from '@/features/cast/api/get-tv-cast';
+import { TopCast } from '@/features/cast/components/top-cast';
+import { isTvOnWatchedQueryOptions } from '@/features/watched/api/is-tv-on-watched';
+import { getTvSeasonDetailsQueryOptions } from '../api/get-tv-season-details';
 
 export const TvView = ({ tvId }: { tvId: string }) => {
+  const queries = useSuspenseQueries({
+    queries: [
+      getTvDetailsQueryOptions({ id: tvId }),
+      isTvOnWatchlistQueryOptions({ id: tvId }),
+      getTvWatchProvidersQueryOptions({ id: tvId }),
+      getTvCastQueryOptions({ id: tvId }),
+      isTvOnWatchedQueryOptions({ id: tvId }),
+      getTvSeasonDetailsQueryOptions({ id: tvId, seasonId: '1' }),
+    ],
+  });
+
+  const headerData: TvHeaderData = {
+    tvDetails: queries[0].data.tv,
+    onWatchlist: queries[1].data.in_watchlist,
+    watchproviders: queries[2].data.watch_providers,
+  };
+
   return (
     <div className="flex flex-col gap-8">
-      <TvHeader tvId={tvId} />
-      <NextEpisode tvId={tvId} />
-      <TvCast tvId={tvId} />
-      <TvSeasons tvId={tvId} />
+      <TvHeader tvId={tvId} data={headerData} />
+      <NextEpisode tvId={tvId} data={queries[4].data.watched_tv} />
+      <TopCast id={tvId} isTv={true} data={queries[3].data.cast} />
+      <TvSeasons tvId={tvId} data={queries[0].data.tv} />
     </div>
   );
 };
